629be00409f0419af41a7bf1e34152a8652f407f
michelolga6-debug <michelolga6@gmail.com>
2026-02-13 19:58:51 -0400

    Practica 14

---
+++ Practica 14/Practica 14/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Practica_14
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabPage2 = new TabPage();
+            tabPage1 = new TabPage();
+            button1 = new Button();
+            tabControl1 = new TabControl();
+            tabPage1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 34);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(492, 267);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Segunda seccion";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(button1);
+            tabPage1.Location = new Point(4, 34);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(492, 267);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Pestaña inicio";
+            tabPage1.Click += tabPage1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(9, 61);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "Haz click";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(233, 30);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(500, 305);
+            tabControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 255);
+            ClientSize = new Size(1251, 794);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabPage1.ResumeLayout(false);
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabPage tabPage2;
+        private TabPage tabPage1;
+        private Button button1;
+        private TabControl tabControl1;
+    }
+}
---
+++ Practica 14/Practica 14/Form1.cs
@@ -0,0 +1,20 @@
+namespace Practica_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 1;
+        }
+    }
+}
