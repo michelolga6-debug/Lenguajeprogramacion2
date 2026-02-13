02c981829de6dc562da2206d3965b9f53e98ac7c
michelolga6-debug <michelolga6@gmail.com>
2026-02-13 19:10:50 -0400

    Practica 10

---
+++ Practica 10/Practica 10/Form1.cs
@@ -0,0 +1,15 @@
+namespace Practica_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola, soy Olga Josefina Michel y mi matricula es SD-2022-03572");
+        }
+    }
+}
