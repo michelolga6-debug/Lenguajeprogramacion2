69fef75f6a7ea5e53048b71113cc3dbed2fb51c9
michelolga6-debug <michelolga6@gmail.com>
2026-02-13 11:13:51 -0400

    Ejercicio1

---
+++ Ejercicio 1/Ejercicio 1/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Ejercicio_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.White;
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(56, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(410, 25);
+            label1.TabIndex = 0;
+            label1.Text = "¡Bienvenidos al formulario de Nombre y Matricula!";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 255, 255);
+            ClientSize = new Size(599, 450);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Formulaio1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
