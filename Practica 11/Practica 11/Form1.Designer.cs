ea8f7f081209459cb96fc0b0f0e6657780b11b63
michelolga6-debug <michelolga6@gmail.com>
2026-02-13 19:20:58 -0400

    Practica 11

---
+++ Practica 11/Practica 11/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Practica_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dateTimePicker1 = new DateTimePicker();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(49, 37);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(342, 31);
+            dateTimePicker1.TabIndex = 0;
+            dateTimePicker1.Value = new DateTime(2026, 2, 13, 0, 0, 0, 0);
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.Location = new Point(445, 37);
+            button1.Name = "button1";
+            button1.Size = new Size(294, 62);
+            button1.TabIndex = 1;
+            button1.Text = "Haz click aqui";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.White;
+            label1.Location = new Point(224, 155);
+            label1.Name = "label1";
+            label1.Size = new Size(289, 84);
+            label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 255);
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(dateTimePicker1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private Button button1;
+        private Label label1;
+    }
+}
